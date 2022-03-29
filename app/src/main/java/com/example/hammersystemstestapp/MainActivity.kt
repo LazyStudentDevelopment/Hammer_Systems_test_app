b416a286f2601d13f5d1faf9c8b2630b7a0e0f76
LazyStudentDevelopment <76945816+LazyStudentDevelopment@users.noreply.github.com>
2022-03-29 18:34:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hammersystemstestapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.hammersystemstestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
